e5dca219dc7c963aa7278a74922608b79aa1deda
Mory Friedman <friedmanmory2@gmail.com>
2016-03-07 22:21:32 -0600

    Mad Lib

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var noun = prompt('Enter a noun');
+var verb = prompt('Enter a verb');
+var adjective = prompt('Enter an adjective');
+var adverb = prompt('Enter an adverb');
+
+document.write('<div>Enter a noun: '+noun+'</div>');
+document.write('<div>Enter a verb: '+verb+'</div>');
+document.write('<div>Enter an adjective: '+adjective+'</div>');
+document.write('<div>Enter an adverb: '+adverb+'</div>');
+document.write('Do you '+verb+' your '+adjective+' '+noun+' '+adverb+' '+'That\'s hilarious!');
